588ff5519ffd18bfb7d89c6b9d87a62919d33be7
SWARAJ_KADAM <72388467+1720-swaraj@users.noreply.github.com>
2026-07-28 14:42:12 +0530

    Configure Vite server settings for development
    Port number changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    port: 5173,
+    strictPort: true,   // fails if port busy, instead of picking another
+    host: true,          // allows access from outside localhost (needed in Docker dev mode)
+  }
 })
